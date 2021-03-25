c646447c401d27e39c1a17f8b27414cf4eb7965b
Seulgi_LEE <dl4459@naver.com>
2021-03-25 22:07:40 +0900

    client_study

--- React/Blog/client/src/redux/sagas/index.js
+++ React/Blog/client/src/redux/sagas/index.js
@@ -9,3 +9,6 @@ axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 export default function* rootSaga() {
   yield all([]);
 }
+// function* => generator function
+// 함수가 특정 지점에서 끝나고 다음 실행 때는 끝난 지점에서 다시 시작
+// yield -> return처럼 함수를 종료
