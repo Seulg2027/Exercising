552d3df7504b45e173b2075294a8e2bdd78c1d11
Seulgi_LEE <dl4459@naver.com>
2021-03-30 16:49:58 +0900

    loginModal update

--- React/Blog/client/src/components/auth/LoginModal.js
+++ React/Blog/client/src/components/auth/LoginModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {
     Alert,
@@ -17,10 +17,35 @@ import { CLEAR_ERROR_REQUEST, LOGIN_REQUEST } from '../../redux/types';
 
 function LoginModal() {
     // useState
-    const [modal, setModal] = useState(false);
+    const [ modal, setModal ] = useState(false);
+    const [ localMsg, setLocalMsg ] = useState("");
+    // 에러를 일으켰을 때 localMsg가 들어감
+    const [form, setValues ] = useState({
+        email: "",
+        password: "",
+    });
     
     const dispatch = useDispatch();
+    const { errorMsg } = useSelector(state => state.auth);
 
+    useEffect(() => {
+        try{
+            setLocalMsg(errorMsg); // errorMsg를 로컬메세지 state에 추가
+        } catch(e){
+            console.log(e);
+        }
+    }, [errorMsg]);
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        const { email, password } = form; 
+        const user = { email, password };
+        
+        dispatch({
+            type: LOGIN_REQUEST,
+            payload: user,
+        });
+    };
     
     // modal을 켜고 끄는 함수!
     // CLEAR_ERROR_REQUEST는 모달에 뜨는 메세지를 지워줌
@@ -29,6 +54,13 @@ function LoginModal() {
             type: CLEAR_ERROR_REQUEST,
         }); //payload는 설정 안해줌
         setModal(!modal); //모달을 키거나 끈다
+    }
+
+    const onChange = (e) =>{
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
     }
 
 
@@ -55,8 +87,58 @@ function LoginModal() {
                     <b>Blog</b>
                 </ModalHeader>
                 <ModalBody>
-                    <Form>
-
+                    { localMsg ? <Alert color="danger">{localMsg}</Alert> : null}
+                    <Form onSubmit={onSubmit}>
+                        <FormGroup> 
+                            <div id="line" className="mb-4" style={style.textLine}>
+                                소셜 계정으로 로그인
+                            </div>
+                            <div id="line" className="mb-2" style={style.textLine}>
+                                이메일로 로그인
+                            </div>
+                            <div style={style.emailLogin}>
+                                <Label for="email">Email</Label>
+                                <Input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder="email"
+                                    onChange={onChange}
+                                />
+                                <Label for="password">Password</Label>
+                                <Input 
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="Password"
+                                    onChange={onChange}
+                                />
+                                <Button color="dark" style={style.loginButton} block>
+                                    LOGIN
+                                </Button>
+                            </div>
+                            <div>
+                                <div
+                                    className="d-flex justify-content-center mt-3"
+                                    style={style.register}>
+                                    <span>
+                                        <a href="/findpassword" className="text-decoration-none">
+                                            Forgot Password?
+                                        </a>
+                                    </span>
+                                </div>
+                                <div
+                                    className="d-flex justify-content-center mt-1"
+                                    style={style.register}>
+                                    <span>
+                                        <span>Not a member?&nbsp;&nbsp; </span>
+                                        <a href="/register" className="text-decoration-none">
+                                            REGISTER
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
+                        </FormGroup>
                     </Form>
                 </ModalBody>
             </Modal>
@@ -68,6 +150,9 @@ const style = {
     loginLink: { fontSize : "1.2rem" },
     modalHeader: { fontSize: "2rem" },
     loginButton: { marginTop: "2rem"},
+    emailLogin: { width: "100%", marginLeft: "2.5%"},
+    textLine: { width: "100%", marginLeft: "0"},
+    register: { fontSize: "0.8rem" },
 };
 
 export default LoginModal;
