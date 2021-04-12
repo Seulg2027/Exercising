74ac76b5582b1d8149bc68012046bd1538f22b0d
Seulgi_LEE <dl4459@naver.com>
2021-04-12 21:05:19 +0900

    update

---
+++ React/Blog/client/src/components/auth/loadUser.js
@@ -0,0 +1,16 @@
+import store from "../../store";
+import { USER_LOADING_REQUEST } from "../../redux/types";
+
+// 이건 functional component가 아니고 그냥 함수!
+function loadUser(){
+    try {
+        store.dispatch({
+            type: USER_LOADING_REQUEST,
+            payload: localStorage.getItem("token"),
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default loadUser;
