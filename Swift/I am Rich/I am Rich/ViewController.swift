1a242ba1183956e58d17588bd4211065de9e740b
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-05-15 00:22:33 +0900

    [swift] section 1,2,3

---
+++ Swift/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 이슬기 on 2022/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
